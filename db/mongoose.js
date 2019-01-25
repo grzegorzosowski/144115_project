95ab252eb128bb69e54922872394a5440e162f66
Grzegorz <osowskigrzegorz95@gmail.com>
2019-01-25 17:18:47 +0100

    repository 3

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Colony', {
+mongoose.connect('mongodb://lolhax18:gregor95!@ds027688.mlab.com:27688/colonycreator', {
     useNewUrlParser: true
 })
     .then(() => console.log('Connected..'))
