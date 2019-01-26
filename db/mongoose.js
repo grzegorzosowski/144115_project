ebc1200c30981b718067253a9ef6ad87601e746c
Grzegorz <osowskigrzegorz95@gmail.com>
2019-01-26 13:16:03 +0100

    repository 4

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://lolhax18:gregor95!@ds027688.mlab.com:27688/colonycreator', {
+mongoose.connect('mongodb://admin:admin12345@cluster0-shard-00-00-rqy2g.mongodb.net:27017,cluster0-shard-00-01-rqy2g.mongodb.net:27017,cluster0-shard-00-02-rqy2g.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', {
     useNewUrlParser: true
 })
     .then(() => console.log('Connected..'))
